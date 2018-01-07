1e3ccd222c73fded4d85b49cc743e3fa3d81dc16
Jeffrey Taylor <jeff@thetaylors.com>
2018-01-07 14:45:58 -0500

    more work on details page

--- client/app.js
+++ client/app.js
@@ -1,17 +1,17 @@
 const Vue = require('vue');
 const VueRouter = require('vue-router');
-const Home = require('./home/home.vue');
-const Details = require('./details/details.vue');
+const Home = require('./home/home.vue').default;
+const Details = require('./details/details.vue').default;
 
 Vue.use(VueRouter);
 
-new Vue({
+export default new Vue({
   el: '#app',
   router: new VueRouter({
     mode: 'history',
     routes: [
-      { path: '/', component: Home },
-      { path: '/details', component: Details }
+      { path: '/', name: 'Home', component: Home },
+      { path: '/details', name: 'Details', component: Details }
     ]
   })
 });
